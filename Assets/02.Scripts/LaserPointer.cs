8c5b7d0fb52ff443505cd90490aa9fb4fc6b682e
Lee JaeHyun <myleje@gmail.com>
2021-12-22 17:25:42 +0900

    .

--- Assets/02.Scripts/LaserPointer.cs
+++ Assets/02.Scripts/LaserPointer.cs
@@ -10,12 +10,14 @@ public class LaserPointer : MonoBehaviour
     [Range(5.0f, 20.0f)]
     public float maxDistance = 10.0f;
 
-    public GameObject laserMaker;
+    public GameObject laserMakerPrefab;
+    private GameObject laserMaker;
 
     // Start is called before the first frame update
     void Start()
     {
         laser = GetComponent<LineRenderer>();
+        laserMaker = Instantiate<GameObject>(laserMakerPrefab);
     }
 
     // Update is called once per frame
@@ -25,10 +27,14 @@ public class LaserPointer : MonoBehaviour
         if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistance))
         {
             laser.SetPosition(1, new Vector3(0, 0, hit.distance));
+            laserMaker.transform.position = hit.point;
+            laserMaker.transform.rotation = Quaternion.LookRotation(hit.normal);
         }
         else
         {
             laser.SetPosition(1, new Vector3(0, 0, maxDistance));
+            laserMaker.transform.position = transform.position + (transform.forward * maxDistance);
+            laserMaker.transform.rotation = Quaternion.LookRotation(transform.forward);
         }
     }
 }
